0025a8e4633c049961520c244f2167bdf9941e87
IlluminatiN1 <patriksvaerd@gmail.com>
2024-10-01 13:25:35 +0200

    added dataModel

---
+++ data/dataModel.ts
@@ -0,0 +1,26 @@
+export interface Round {
+    shots: number[];
+    total: number;
+    roundNumber: number;
+}
+export interface Training {
+    id: string;
+    date: string;
+    location: string;
+    firstPeriod: Round[];
+    firstPeriodTotalScore: number;
+    secondPeriod: Round[];
+    secondPeriodTotalScore: number;
+    totalScore: number;
+}
+export interface Competition {
+    id: string;
+    name: string;
+    date: string;
+    location: string;
+    firstPeriod: Round[];
+    firstPeriodTotalScore: number;
+    secondPeriod: Round[];
+    secondPeriodTotalScore: number;
+    totalScore: number;
+}
